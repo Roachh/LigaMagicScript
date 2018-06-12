fa9841e9a92d05447b2bb1fb4973879f9d01c5db
Thiago Estácio Barata <thgebarata@gmail.com>
2018-06-12 10:35:43 -0300

    linhas em objeto e remove promocao

--- Script.js
+++ Script.js
@@ -9,7 +9,14 @@ console.log(cardName);
 let url = "https://www.ligamagic.com.br/?view=cards%2Fsearch&card=" + cardName;
 let nomesLojas = [];
 let precosCartas = [];
-//let url = "https://www.ligamagic.com.br/?view=cards%2Fsearch&card=Akoum%20Refuge";
+let estoquesLinhas = [];
+
+class EstoqueLinha {
+  constructor(nome, preco) {
+    this.nome = nome;
+    this.preco = preco;
+  }
+}
 
 
 request(url, function(err, resp, body) {
@@ -17,41 +24,53 @@ request(url, function(err, resp, body) {
     console.log(err);
   } else {
     let $ = cheerio.load(body);
-    let estoquesLinhas = [];
-    estoquesLinhas = $('.estoque-linha').each(function() {
+    let estoquesLinhasCheerio = [];
+
+    let count = 0;
+    estoquesLinhasCheerio = $('.estoque-linha').each(function() {
       let nomeLoja = $(this).find('.e-col1 img').attr('title');
+      let precoCartaPromo = $(this).find('.e-col3').text();
+      let precoCarta = removePromo(precoCartaPromo);
 
-      let precoCartaPromo = $(this).find('.e-col3').html();
 
-
-      if(!nomesLojas.includes(nomeLoja) && nomeLoja != null) {
-        nomesLojas.push(nomeLoja);
-        precosCartas.push(precoCartaPromo);
+      if(!contem(estoquesLinhas, nomeLoja) && nomeLoja != null) {
+        // nomesLojas.push(nomeLoja);
+        // precosCartas.push(precoCarta);
+        estoquesLinhas[count] = new EstoqueLinha(nomeLoja, precoCarta);
+        count++;
       }
-
-
 
     });
 
 
-    console.log(nomesLojas);
-    console.log(precosCartas);
+    console.log(estoquesLinhas);
 
 
-
-
-
-
-    // let estoquesLinhas = [];
-    // estoquesLinhas = $('.estoque-linha .e-col1 img').each(function() {
-    //   let nomeLoja = $(this).attr('title');
-    //
-    //
-    //   if(!nomesDasLojas.includes(nomeLoja)) {
-    //     nomesDasLojas.push(nomeLoja);
-    //   }
-    //
-    // });
-    // console.log(nomesDasLojas);
   }
 });
+
+
+
+function removePromo(precoCartaPromo) {
+  let flag = 0;
+  let subsStart = 0;
+  for (var i = 0; i < precoCartaPromo.length; i++) {
+    if(precoCartaPromo[i] == 'R') {
+      if(flag == 1) {
+        subsStart = i;
+      }
+      flag += 1;
+    }
+  }
+  return precoCartaPromo = precoCartaPromo.substring(subsStart, precoCartaPromo.length);
+}
+
+function  contem(estoquesLinhas, nomeLoja) {
+  let found = false;
+  for (var i = 0; i < estoquesLinhas.length; i++) {
+    if(estoquesLinhas[i].nome == nomeLoja) {
+      found = true;
+    }
+  }
+    return found;
+}
