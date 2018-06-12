3f6900e8d6a515d95bcc78e3e94b74139aa04a23
Thiago Estácio Barata <thgebarata@gmail.com>
2018-06-12 14:56:08 -0300

    Versao com request assincrono, nao funcionando

--- Script.js
+++ Script.js
@@ -3,13 +3,10 @@ let request = require("request");
 let striptags = require('striptags');
 
 
-let cardName = "Akoum Refuge";
-cardName = encodeURIComponent(cardName.trim());
-console.log(cardName);
-let url = "https://www.ligamagic.com.br/?view=cards%2Fsearch&card=" + cardName;
-let nomesLojas = [];
-let precosCartas = [];
-let estoquesLinhas = [];
+let cardName;
+let url;
+let linhasCartas = [];
+let resultadosLojas = [];
 
 class EstoqueLinha {
   constructor(nome, preco) {
@@ -18,36 +15,87 @@ class EstoqueLinha {
   }
 }
 
+class ResultadoLoja {
+  constructor(nome, precoTotal) {
+    this.nome = nome;
+    this.precoTotal = precoTotal;
+  }
+}
 
-request(url, function(err, resp, body) {
-  if(err) {
-    console.log(err);
-  } else {
-    let $ = cheerio.load(body);
-    let estoquesLinhasCheerio = [];
-
-    let count = 0;
-    estoquesLinhasCheerio = $('.estoque-linha').each(function() {
-      let nomeLoja = $(this).find('.e-col1 img').attr('title');
-      let precoCartaPromo = $(this).find('.e-col3').text();
-      let precoCarta = removePromo(precoCartaPromo);
+cardName = "Akoum Refuge";
+console.log(cardName);
+linhasCartas.push(linhasCarta(cardName));
 
 
-      if(!contem(estoquesLinhas, nomeLoja) && nomeLoja != null) {
-        // nomesLojas.push(nomeLoja);
-        // precosCartas.push(precoCarta);
-        estoquesLinhas[count] = new EstoqueLinha(nomeLoja, precoCarta);
-        count++;
+cardName = "Battlefield Forge";
+console.log(cardName);
+linhasCartas.push(linhasCarta(cardName));
+//console.log(linhasCartas);
+
+
+
+for (var i = 0; i < linhasCartas.length; i++) {
+  let flag = 0;
+  let index = 0;
+  for (var i2 = 0; i2 < resultadosLojas.length; i2++) {
+    //já adicionou essa loja nos resultados
+    if(resultadosLojas[i2].nome == linhasCartas[i].nome) {
+      flag = 1;
+      index = i2;
+    }
+  }
+  //se não adicionou, adiciona com o nome e preço. Se adicionou, soma o preço no resultadosLojas[index]
+  // if(flag == 0) {
+  //   let resultadoLoja = new ResultadoLoja(linhasCartas[i].nome, linhasCartas[i].preco)
+  //   resultadosLojas.push(resultadoLoja);
+  //   console.log('Criou!');
+  // } else {
+  //   resultadosLojas[index].preco += linhasCartas[i].preco;
+  //   console.log('Somou!');
+  // }
+}
+
+
+
+
+
+
+function linhasCarta(cardName) {
+  cardName = encodeURIComponent(cardName.trim());
+  url = "https://www.ligamagic.com.br/?view=cards%2Fsearch&card=" + cardName;
+  let estoquesLinhas = [];
+
+  //assincrono start
+  request(url, function(err, resp, body) {
+    if(err) {
+      console.log(err);
+    } else {
+      let $ = cheerio.load(body);
+      let estoquesLinhasCheerio = [];
+
+      estoquesLinhasCheerio = $('.estoque-linha').each(function() {
+        let nomeLoja = $(this).find('.e-col1 img').attr('title');
+        let precoCartaPromo = $(this).find('.e-col3').text();
+        let precoCarta = removePromo(precoCartaPromo);
+
+
+        if(!contem(estoquesLinhas, nomeLoja) && nomeLoja) {
+          estoquesLinhas.push(new EstoqueLinha(nomeLoja, precoCarta));
+          //console.log(estoquesLinhas[count]);
+        }
       }
-
-    });
-
+    );
 
     console.log(estoquesLinhas);
+    //console.log('\n');
 
 
   }
-});
+});// assincrono end
+
+//console.log(estoquesLinhas);
+return estoquesLinhas;
+}
 
 
 
@@ -72,5 +120,5 @@ function  contem(estoquesLinhas, nomeLoja) {
       found = true;
     }
   }
-    return found;
+  return found;
 }
