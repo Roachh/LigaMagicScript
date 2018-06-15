4d1af9b7ee83de8c9687da3d69d28afdb2844de2
Thiago Barata <thiagob@triad.local>
2018-06-15 15:32:31 -0300

    identa mentos e var para let

--- Script.js
+++ Script.js
@@ -6,13 +6,10 @@ const fs = require('fs');
 let cardsNames = [];
 let promisesLinhasCartas = [];
 let url;
-let linhasCartas = [];
 let resultadosLojas = [];
-let loadingCount = 0;
 
 
-
-var lineReader = readline.createInterface({
+let lineReader = readline.createInterface({
   input: fs.createReadStream(process.cwd() + '\\cartas.txt')
 });
 
@@ -20,9 +17,9 @@ lineReader.on('line', function (line) {
   cardsNames.push(line);
 });
 
-lineReader.on('close', function() {
+lineReader.on('close', function () {
   execute();
-})
+});
 
 
 class EstoqueLinha {
@@ -40,35 +37,40 @@ class ResultadoLoja {
   }
 }
 
-function execute(){
+function execute() {
+  let linhasCartas = [];
+  let loadingCount = 0;
+
   fs.writeFileSync(process.cwd() + '\\resultado.txt', '');
   fs.appendFileSync(process.cwd() + '\\resultado.txt', "Total de cartas: " + cardsNames.length + '\r\n \r\n');
-  for (var i = 0; i < cardsNames.length; i++) {
-    promisesLinhasCartas[i] = linhasCarta(cardsNames[i]).then(function(estoquesLinhas) {
+  
+  for (let i = 0; i < cardsNames.length; i++) {
+    
+    promisesLinhasCartas[i] = linhasCarta(cardsNames[i]).then(function (estoquesLinhas) {
       linhasCartas.push(estoquesLinhas);
       loadingCount++;
       printProgress(loadingCount, cardsNames.length);
     });
   }
 
-  
 
-  Promise.all(promisesLinhasCartas).then(function() {
+
+  Promise.all(promisesLinhasCartas).then(function () {
     //console.log(linhasCartas[0][0].nome);
-    for (var i = 0; i < linhasCartas.length; i++) {
-      for (var i2 = 0; i2 < linhasCartas[i].length; i2++) {
+    for (let i = 0; i < linhasCartas.length; i++) {
+      for (let i2 = 0; i2 < linhasCartas[i].length; i2++) {
 
         let flag = 0;
         let index;
-        for (var i3 = 0; i3 < resultadosLojas.length; i3++) {
+        for (let i3 = 0; i3 < resultadosLojas.length; i3++) {
           //já adicionou essa loja nos resultados
-          if(resultadosLojas[i3].nome == linhasCartas[i][i2].nome) {
+          if (resultadosLojas[i3].nome == linhasCartas[i][i2].nome) {
             flag = 1;
             index = i3;
           }
         }
         //se não adicionou, adiciona com o nome e preço. Se adicionou, soma o preço no resultadosLojas[index]
-        if(flag == 0) {
+        if (flag == 0) {
           let resultadoLoja = new ResultadoLoja(linhasCartas[i][i2].nome, linhasCartas[i][i2].preco);
           resultadoLoja.qtdCartas += 1;
           resultadosLojas.push(resultadoLoja);
@@ -80,10 +82,10 @@ function execute(){
         }
       }
     }
-    resultadosLojas.sort(function(a, b){
+    resultadosLojas.sort(function (a, b) {
       return b.qtdCartas - a.qtdCartas;
     });
-    for (var i = 0; i < resultadosLojas.length; i++) {
+    for (let i = 0; i < resultadosLojas.length; i++) {
       process.stdout.write("Nome da Loja: " + resultadosLojas[i].nome + ' | ');
       process.stdout.write("Quantidade de Cartas: " + resultadosLojas[i].qtdCartas + ' | ');
       process.stdout.write("Preço Total: " + resultadosLojas[i].precoTotal.toFixed(2) + '\n \n');
@@ -104,25 +106,25 @@ function linhasCarta(cardName) {
   url = "https://www.ligamagic.com.br/?view=cards%2Fsearch&card=" + encodedCardName;
   let estoquesLinhas = [];
 
-  
+
 
   //assincrono start
-  return request( { uri: url, timeout: 30000 } ).then(function(body) {
+  return request({ uri: url, timeout: 30000 }).then(function (body) {
 
     let $ = cheerio.load(body);
     let estoquesLinhasCheerio = [];
 
-    if($('.estoque-linha').length !== 0) {
+    if ($('.estoque-linha').length !== 0) {
 
-      $('.estoque-linha').each(function() {
+      $('.estoque-linha').each(function () {
         let nomeLoja = $(this).find('.e-col1 img').attr('title');
         let precoCartaWithPromo = $(this).find('.e-col3').text();
         let precoCarta = getPromo(precoCartaWithPromo);
-        precoCarta = precoCarta.replace(',','.');
+        precoCarta = precoCarta.replace(',', '.');
         precoCarta = Number(precoCarta.slice(2));
 
 
-        if(!contem(estoquesLinhas, nomeLoja) && nomeLoja) {
+        if (!contem(estoquesLinhas, nomeLoja) && nomeLoja) {
           estoquesLinhas.push(new EstoqueLinha(nomeLoja, precoCarta));
         }
       });
@@ -142,32 +144,32 @@ function linhasCarta(cardName) {
 function getPromo(precoCartaWithPromo) {
   let flag = 0;
   let subsStart = 0;
-  for (var i = 0; i < precoCartaWithPromo.length; i++) {
-    if(precoCartaWithPromo[i] == 'R') {
-      if(flag == 1) {
+  for (let i = 0; i < precoCartaWithPromo.length; i++) {
+    if (precoCartaWithPromo[i] == 'R') {
+      if (flag == 1) {
         subsStart = i;
       }
       flag += 1;
     }
   }
-  return precoCartaWithPromo = precoCartaWithPromo.substring(subsStart, precoCartaWithPromo.length);
+  return precoCartaWithPromo.substring(subsStart, precoCartaWithPromo.length);
 }
 
-function  contem(estoquesLinhas, nomeLoja) {
+function contem(estoquesLinhas, nomeLoja) {
   let found = false;
-  for (var i = 0; i < estoquesLinhas.length; i++) {
-    if(estoquesLinhas[i].nome == nomeLoja) {
+  for (let i = 0; i < estoquesLinhas.length; i++) {
+    if (estoquesLinhas[i].nome == nomeLoja) {
       found = true;
     }
   }
   return found;
 }
 
-function printProgress(loadingCount, totalCartas){
+function printProgress(loadingCount, totalCartas) {
   process.stdout.clearLine();
   process.stdout.cursorTo(0);
   process.stdout.write('Carregando: ' + loadingCount + '/' + totalCartas);
-  if(loadingCount == totalCartas) {
+  if (loadingCount == totalCartas) {
     process.stdout.write('\n');
   }
 }
